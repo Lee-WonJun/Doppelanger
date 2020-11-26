c7d79de01e50a4602cd67eab478961b9c8b0b715
Lee-WonJun <dldnjs1013@gmail.com>
2020-11-27 01:41:45 +0900

    Routing Test

--- doppelanger-server/project.clj
+++ doppelanger-server/project.clj
@@ -6,7 +6,8 @@
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]]
   :plugins [[lein-ring "0.12.5"]]
-  :ring {:handler doppelanger-server.handler/app}
+  :ring {:nrepl {:start? true :port 4001}
+         :handler doppelanger-server.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.2"]]}})
--- doppelanger-server/src/doppelanger_server/handler.clj
+++ doppelanger-server/src/doppelanger_server/handler.clj
@@ -4,8 +4,12 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/not-found "Not Found"))
+           (GET "/" [] "Hello World")
+           (GET "/search" [domain keyword target] (str domain "," keyword "->" target))
+           (GET "/user/:id" [id greeting]
+             (str "<h1>" greeting " user " id "</h1>"))
+           (route/not-found "Not Found"))
 
 (def app
   (wrap-defaults app-routes site-defaults))
+
