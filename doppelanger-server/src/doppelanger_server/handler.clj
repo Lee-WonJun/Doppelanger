f1b99ff68680a05845f64e0bc9808dce8855eff1
Lee-WonJun <dldnjs1013@gmail.com>
2020-11-27 19:44:43 +0900

    Route basic path

--- doppelanger-server/src/doppelanger_server/handler.clj
+++ doppelanger-server/src/doppelanger_server/handler.clj
@@ -3,13 +3,18 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(defn search [domain keyword target]
+  (str domain keyword target))
+
+(defn match [] "something")
+
 (defroutes app-routes
            (GET "/" [] "Hello World")
-           (GET "/search" [domain keyword target] (str domain "," keyword "->" target))
-           (GET "/user/:id" [id greeting]
-             (str "<h1>" greeting " user " id "</h1>"))
+           (GET "/search" [domain keyword target] (search domain keyword target))
+
+           (POST "/match" {body :body}  body)
            (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-defaults app-routes (assoc site-defaults :security false)))
 
--- doppelanger-server/test/doppelanger_server/handler_test.clj
+++ doppelanger-server/test/doppelanger_server/handler_test.clj
@@ -1,7 +1,8 @@
 (ns doppelanger-server.handler-test
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
-            [doppelanger-server.handler :refer :all]))
+            [doppelanger-server.handler :refer :all]
+            [cheshire.core :as json]))
 
 (deftest test-app
   (testing "main route"
@@ -11,4 +12,19 @@
 
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+      (is (= (:status response) 404))))
+
+  (testing "search route"
+    (let [query {:domain "clojure" :keyword "frequencies" :target "F#" }
+          response (app (-> (mock/request :get "/search") (mock/query-string query)))]
+      (is (= (:status response) 200))))
+
+  (testing "post route"
+    (let [[domain keyword target-domain target-keyword] ["haskell" "do-notation" "scala" "for-notation"]
+          handler (-> (mock/body (mock/request :post "/match")
+                                 (json/generate-string {:domain domain :keyword keyword :target-domain target-domain :target-keyword target-keyword}))
+                      (mock/content-type "application/json"))
+          _ (println handler)
+          response (app handler)
+          _ (println response)]
+    (is (= (:status response) 200)))))
