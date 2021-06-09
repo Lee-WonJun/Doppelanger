a424a40c1f94942f45d52fb6b0a0570ce1f4aa85
DESKTOP-7LJV0BF\dldnj <dldnjs1013@nate.com>
2021-06-10 02:20:05 +0900

    Write DB Sql for H2 database

--- resources/migrations/20210102000823-add-users-table.up.sql
+++ resources/migrations/20210102000823-add-users-table.up.sql
@@ -1,9 +1,46 @@
-CREATE TABLE users
-(id VARCHAR(20) PRIMARY KEY,
- first_name VARCHAR(30),
- last_name VARCHAR(30),
- email VARCHAR(30),
- admin BOOLEAN,
- last_login TIMESTAMP,
- is_active BOOLEAN,
- pass VARCHAR(300));
+--CREATE TABLE users
+--(id VARCHAR(20) PRIMARY KEY,
+-- first_name VARCHAR(30),
+-- last_name VARCHAR(30),
+-- email VARCHAR(30),
+-- admin BOOLEAN,
+-- last_login TIMESTAMP,
+-- is_active BOOLEAN,
+-- pass VARCHAR(300));
+
+-- 테이블 순서는 관계를 고려하여 한 번에 실행해도 에러가 발생하지 않게 정렬되었습니다.
+
+-- DOMAINS Table Create SQL
+CREATE TABLE DOMAINS
+(
+    Domain  VARCHAR(45) PRIMARY KEY NOT NULL
+);
+
+
+-- KEYWORDS Table Create SQL
+CREATE TABLE KEYWORDS
+(
+    KeywordID  INT            NOT NULL   PRIMARY KEY  AUTO_INCREMENT,
+    Domain     VARCHAR(45)    NOT NULL,
+    Keyword   VARCHAR(45)    NOT NULL
+);
+
+ALTER TABLE KEYWORDS
+    ADD FOREIGN KEY (Domain)
+        REFERENCES DOMAINS (Domain);
+
+
+-- RELATIONS Table Create SQL
+CREATE TABLE KEYWORD_RELATIONS
+(
+    idx        INT            NOT NULL  PRIMARY KEY   AUTO_INCREMENT,
+    KeywordID      INT            NOT NULL,
+    RelationGroup  VARCHAR(45)    NOT NULL
+);
+
+
+ALTER TABLE KEYWORD_RELATIONS
+    ADD FOREIGN KEY (KeywordID)
+        REFERENCES KEYWORDS (KeywordID);
+
+
--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,21 +1,65 @@
--- :name create-user! :! :n
+---- :name create-user! :! :n
+---- :doc creates a new user record
+--INSERT INTO users
+--(id, first_name, last_name, email, pass)
+--VALUES (:id, :first_name, :last_name, :email, :pass)
+--
+---- :name update-user! :! :n
+---- :doc updates an existing user record
+--UPDATE users
+--SET first_name = :first_name, last_name = :last_name, email = :email
+--WHERE id = :id
+--
+---- :name get-user :? :1
+---- :doc retrieves a user record given the id
+--SELECT * FROM users
+--WHERE id = :id
+--
+---- :name delete-user! :! :n
+---- :doc deletes a user record given the id
+--DELETE FROM users
+--WHERE id = :id
+
+-- :name get-domain :? :*
+-- :doc select all domains
+SELECT * FROM DOMAINS
+
+-- :name get-keyword-id :? :*
+-- :doc select all domains
+SELECT KeywordID FROM KEYWORDS
+WHERE Keyword = :keyword
+
+-- :name create-domain! :! :n
 -- :doc creates a new user record
-INSERT INTO users
-(id, first_name, last_name, email, pass)
-VALUES (:id, :first_name, :last_name, :email, :pass)
+INSERT INTO DOMAINS
+(Domain)
+VALUES (:domain)
 
--- :name update-user! :! :n
--- :doc updates an existing user record
-UPDATE users
-SET first_name = :first_name, last_name = :last_name, email = :email
-WHERE id = :id
 
--- :name get-user :? :1
--- :doc retrieves a user record given the id
-SELECT * FROM users
-WHERE id = :id
+-- :name create-keyword! :! :n
+-- :doc creates a new user record
+INSERT INTO KEYWORDS
+(Domain, Keyword)
+VALUES (:domain, :keyword)
 
--- :name delete-user! :! :n
--- :doc deletes a user record given the id
-DELETE FROM users
-WHERE id = :id
+-- :name create-relation! :! :n
+-- :doc creates a new user record
+INSERT INTO KEYWORD_RELATIONS
+(KeywordID, RelationGroup)
+VALUES (:keyword_id, :relation_group)
+
+
+
+-- :name get-relation-groups :! :*
+-- :doc select relation-groups about keyword id
+SELECT DISTINCT r.RelationGroup
+FROM KEYWORD_RELATIONS AS r
+    INNER JOIN KEYWORDS AS k ON r.KeywordID = k.KeywordID
+WHERE r.KeywordID = :keyword_id
+
+-- :name get-dopple-keyword :! :1
+-- :doc select keyword of same mean
+SELECT DISTINCT k.Keyword
+FROM KEYWORD_RELATIONS AS r
+    INNER JOIN KEYWORDS AS k ON r.KeywordID = k.KeywordID
+WHERE r.RelationGroup = :relation_group AND k.Domain = :domain
